b343afb29cab235483fb0f68da9bbd8d8b6fcb5e
boryand <boryand>
2022-05-26 12:58:19 +0200

    changes

--- src/components/kitchen.js
+++ src/components/kitchen.js
@@ -1,37 +1,83 @@
-import food10 from "../media/food10.png";
-import food11 from "../media/food11.png";
-
-import food12 from "../media/food12.png";
-
-import food13 from "../media/food13.png";
-import food1 from "../media/food1.jpeg";
-
-import food2 from "../media/food2.jpeg"
-import food3 from "../media/food3.jpeg"
-import food4 from "../media/food4.jpeg"
-import food5 from "../media/food5.jpeg"
-import food6 from "../media/food6.jpeg"
 const Kitchen = () => {
   return (
     <div className="kitchen_container">
       <div className="kitchen_images">
-        <img className="kitchen_images_items" src={food10} alt="" />
-        <img className="kitchen_images_items" src={food11} alt="" />
-        <img className="kitchen_images_items" src={food12} alt="" />
+        <img
+          className="kitchen_images_items"
+          src={
+            "https://res.cloudinary.com/bobchi/image/upload/v1653561884/food1_jxdyay.jpg"
+          }
+          alt=""
+        />
+        <img
+          className="kitchen_images_items"
+          src={
+            "https://res.cloudinary.com/bobchi/image/upload/v1653561884/food2_bvnmge.jpg"
+          }
+          alt=""
+        />
+        <img
+          className="kitchen_images_items"
+          src={
+            "https://res.cloudinary.com/bobchi/image/upload/v1653561884/food6_utexdu.jpg"
+          }
+          alt=""
+        />
       </div>
       <div className="kitchen_images">
-        <img className="kitchen_images_items" src={food13} alt="" />
-        <img className="kitchen_images_items" src={food1} alt="" />
-        <img className="kitchen_images_items" src={food2} alt="" />
+        <img
+          className="kitchen_images_items"
+          src={
+            "https://res.cloudinary.com/bobchi/image/upload/v1653561884/food5_bsnhfh.jpg"
+          }
+          alt=""
+        />
+        <img
+          className="kitchen_images_items"
+          src={
+            "https://res.cloudinary.com/bobchi/image/upload/v1653561884/food4_oirk8q.jpg"
+          }
+          alt=""
+        />
+        <img
+          className="kitchen_images_items"
+          src={
+            "https://res.cloudinary.com/bobchi/image/upload/v1653561897/food10_eaxsqa.png"
+          }
+          alt=""
+        />
       </div>
       <div className="kitchen_images">
-        <img className="kitchen_images_items" src={food3} alt="" />
-        <img className="kitchen_images_items" src={food4} alt="" />
-        <img className="kitchen_images_items" src={food5} alt="" />
+        <img
+          className="kitchen_images_items"
+          src={
+            "https://res.cloudinary.com/bobchi/image/upload/v1653561884/food3_unegpg.jpg"
+          }
+          alt=""
+        />
+        <img
+          className="kitchen_images_items"
+          src={
+            "https://res.cloudinary.com/bobchi/image/upload/v1653561896/food13_to3bll.png"
+          }
+          alt=""
+        />
+        <img
+          className="kitchen_images_items"
+          src={
+            "https://res.cloudinary.com/bobchi/image/upload/v1653561899/food12_dvgpuy.png"
+          }
+          alt=""
+        />
       </div>
       <div className="kitchen_images">
-        <img className="kitchen_images_items" src={food6} alt="" />
-        
+        <img
+          className="kitchen_images_items"
+          src={
+            "https://res.cloudinary.com/bobchi/image/upload/v1653561903/food11_brefgj.png"
+          }
+          alt=""
+        />
       </div>
     </div>
   );
